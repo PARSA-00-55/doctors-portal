25ae22ccf336821b6cebffc35727fd6a796250e1
Lamp <parsa02345@gmail.com>
2022-12-25 21:14:33 +0600

    loaging bug fixed

--- src/Pages/Shared/Loading/Loading.js
+++ src/Pages/Shared/Loading/Loading.js
@@ -7,7 +7,7 @@ const Loading = () => {
         class="spinner-border animate-spin inline-block w-8 h-8 border-4 rounded-full"
         role="status"
       >
-        <span class="visually-hidden">Loading...</span>
+        <span class="visually-hidden"></span>
       </div>
     </div>
   );
